c01f6eaddd8fe7426e31d4accbccdd1698f17821
nerea pardo <nerea.pardo.cundins@gmail.com>
2021-11-08 17:36:15 +0000

    lab solved

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+app.use(express.static("public"));
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/views/index.html");
+});
+app.get("/about", (request, response) => {
+  response.sendFile(__dirname + "/views/about.html");
+});
+app.get("/works", (request, response) => {
+  response.sendFile(__dirname + "/views/works.html");
+});
+app.get("/gallery", (request, response) => {
+  response.sendFile(__dirname + "/views/gallery.html");
+});
+app.listen(3000, () => {});
